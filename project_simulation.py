4f672b6c148f77bb07611ec85b8c6f0226295928
Saavcamilo <csaavedr@caltech.edu>
2018-11-29 20:51:55 -0800

    Added functional setup

--- project_simulation.py
+++ project_simulation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# -*- coding: cp1252 -*-
 import random
 import collections
 from math import *
@@ -18,19 +18,23 @@ uCox = 50e-6
 uCox_p = 25e-6
 outputCurrent, outputVoltages, outputParams, outputPerform = [], [], [], []
 
+# Capacitance from photodiode
 Cin = 100 * 10 ** -15
-# Capacitance constants
+# Capacitance constants (back calculation to match SPICE)
 Cgate = 2.03 * 10 ** -15
+# Capacitance of load
+Cload = 250.e-15
+# Impedance of load
+Rload = 20.e3
 # Ratio of parasitic capacitance
-Cdb = .33
-# Assume Csb = Cdb 
-Csb = .33
-Cgd = .25
-
+Cdb = 0.33
+Csb = 0.33 # Assume Csb = Cdb 
+Cgd = 0.25
+FOM_Max = 0
 # Sweep pvbias range:
 Vbias_min = -1.9
 Vbias_max = -1.4
-Vbias_inc = .00001
+Vbias_inc = .01
 Vbias = Vbias_min
 
 Vbiasp_min = 1.4
@@ -53,10 +57,12 @@ W_L_3bias = 32.
 Amax = -1000.
 # Loop through Vbiasp, determine current down
 while(Vbiasp < Vbiasp_max):
-    vovLoad = Vdd - Vbiasp - .5
-    IdL = .5 * uCox_p * W_L_1L * vovLoad ** 2
+    vovLoad = Vdd - Vbiasp - V_th
+    IdL = V_th * uCox_p * W_L_1L * vovLoad ** 2
     # RoL is given by IdL
     roL = 1/(lam * IdL)
+    Vbias = Vbias_min
+    print Vbiasp
     #Loop through bottom biasing
     while(Vbias < Vbias_max):
         vovB = Vbias - Vss - V_th    
@@ -73,15 +79,17 @@ while(Vbiasp < Vbiasp_max):
         if Vd1 > 2.5 or Vd1 < 0:
             Vbias += Vbias_inc
             continue
-        vov1 = -Vs1-.5
-        gmL = (2*IdL)/vovLoad
-        gm1 = (2*IdB1)/(vov1)
+        vov1 = -Vs1 - V_th
+        gmL = (2 * IdL)/vovLoad
+        gm1 = (2 * IdB1)/(vov1)
+        gmb1 = gm1 * gam / (2 * sqrt(Vsb + 2 * phi))
         # Use vovPmos and Itop to determine W of M_L1
         Iu = (Vdd - Vd1)/RU
         Id = (Vd1 - Vss)/RD
         # Rtot = roL//Ru//Rd
         Rtot = roL * RU * RD / (roL * RU + roL * RD + RU * RD)
         A1 = -Rtot
+
         # Bandwidth Calculations for Stage 1
         # Calculate Cgs1 from tech. parameters
         Cgs1 = W_L_1 * Cgate
@@ -96,15 +104,18 @@ while(Vbiasp < Vbiasp_max):
         Cgs1B = W_L_1bias * Cgate
         Cdb1B = Cgs1B * Cdb
         Cgd1B = Cgs1B * Cgd
-        CtotB = Cgd1B + Cdb1B 
-        Ctot = Cgs1 + Csb1 + CtotB + Cin
-        #print 1/(Ctot * R3db * 2 * pi * 10 ** 6)
+        Ctot1B = Cgd1B + Cdb1B 
+        Ctot1 = Cgs1 + Csb1 + Ctot1B + Cin
+
+        BW0 = 1/(Ctot1 * R3db * 2 * pi)
+        
         # Approximate other caps as ratio with Cgs1 for simplicity
 
         Cgs1L = W_L_1L * Cgate
         Cdb1L = Cgs1L * Cdb
         Csb1L = Cgs1L * Csb
         Cgd1L = Cgs1L * Cgd
+        
         #PHASE 2
         V2G = Vd1
         Id2 = 0.5 * uCox * W_L_2bias * (Vbias - Vss - V_th) ** 2 # Current set by Vbias
@@ -112,10 +123,7 @@ while(Vbiasp < Vbiasp_max):
         roB2 = 1 / (lam * Id2)
         ro2 = 1 / (lam * Id2)
 
-
-        #Vg2 = Vov2 + Id2 * roB2 + V_th # used to find Vds2
         Vov2L = sqrt(2 * Id2 / (W_L_2L * uCox))
-        #print "top transistor Vov:" + str(Vov2L)
         Vd2 = Vdd - (Vov2L + V_th)
         
         Vov2bias = sqrt(2 * Id2 / (W_L_2bias * uCox))
@@ -129,30 +137,35 @@ while(Vbiasp < Vbiasp_max):
         Vgs2 = Vov2 + V_th2
         Vs2 = V2G - Vgs2
       
-        #print "Vs2: " + str(Vs2)
-        #print "Vgs2: " + str(Vgs2)
         gm2L = 2 * Id2 / (Vdd - Vd2 - V_th)
         gm2 = 2 * Id2 / Vov2
+        gmb2 = gm2 * gam / (2 * sqrt(Vsb2 + 2 * phi)) # not used
         Rl = 1/gm2L * roL2 * ro2 / (1/gm2L * roL2 + roL2 * ro2 + ro2 * 1/gm2L)
         A2 = -gm2 * Rl
         Vds2 = Vd2 - Vs2
         if Vov2 > Vds2: # in saturation
              Vbias += Vbias_inc
              continue
+
         # Phase 2 Capacitance Calculations:
         Cgs2 = W_L_2 * Cgate
-        # Approximate other caps as ratio with Cgs1 for simplicity
+        # Approximate other caps as ratio with Cgs2 for simplicity
         Cdb2 = Cgs2 * Cdb
         Csb2 = Cgs2 * Csb
         Cgd2 = Cgs2 * Cgd
+
+        Cgs2L = W_L_2L * Cgate
+        Cdb2L = Cgs2L * Cdb
+        Csb2L = Cgs2L * Csb
+        Cgd2L = Cgs2L * Cgd
         # Other node of stage 1/Stage2:
 
         Cl = Cgd1L + Cdb1L+ Cgd1 + Cdb1 + Cgs2 + Cgd2 *(1-A2)
         Ra = (RU * RD)/(RU + RD)
-        Rnode2 = (roL * Ra) / (roL + Ra)
+        Rnode2 = Ra
         #print Rnode2, roL, Ra
-        print Cl, Rnode2
-        print "HI", 1/(Cl * Rnode2 * 2 * pi * 10 ** 6) 
+      
+        BW1 = 1/(Cl * Rnode2 * 2 * pi) 
 
         # Phase 3
         V3G = Vd2
@@ -170,26 +183,57 @@ while(Vbiasp < Vbiasp_max):
         Vgs3 = Vov3 + V_th3
         Vs3 = V3G - Vgs3
         Vds3 = Vdd - Vs3
-        # print "Vs3: " + str(Vs3)
-        # print "Vds3: " + str(Vds3)
 
         gm3 = 2 * Id3 / Vov3
         gmb3 = gm3 * gam / (2 * sqrt(Vsb3 + 2 * phi ))
-        Rl = roB3 * ro3 / (roB3 + ro3)
-        A3 = gm3 * Rl / (1 + (gm3 + gmb3) * Rl) 
+        Rl3 = roB3 * ro3 / (roB3 + ro3)
+        Rl3 = Rl3 / (Rl3 * gmb3 + 1)
+        A3 = gm3 * Rl3 / (1 + gm3 * Rl3) 
+
         if Vov3 > Vds3:
               Vbias += Vbias_inc
               continue
+        # Find final gain
         Atot = A1 * A2 * A3
-        print Atot, Amax
+        #print A1, A2, A3, Atot
+        # Keep track of largest gain
         if Atot > Amax:
             Amax = Atot
-            #print "A", Atot, A1, A2, A3
-            #print "Vbias ", Vbias, "Vbiasp ", Vbiasp
+
         # Phase 3 Capacitance Calculations
         Cgs3 = W_L_3 * Cgate
-        Cds3 = Cgs3 * Cds
         Cgd3 = Cgs3 * Cgd
-        Cds3 = Cds3 * Cds  
+        Csb3 = Cgs3 * Csb  
+
+        Cgs3B = W_L_3bias * Cgate
+        Cdb3B = Cgs3B * Cdb
+        Cgd3B = Cgs3B * Cgd
+
+        #Bandwidth from Stage 2 to 3
+        
+        C2pole = Cgd2 * (1. - 1. / A2) + Cdb2 + Cgs2L + Csb2L + Cgd3 + Cgs3 * (1 - A3) 
+        R2L = roL2 / (roL2 * gm2L + 1)
+        R2pole = ro2 * R2L / (ro2 + R2L)
+
+        BW2 = 1/(C2pole * R2pole * 2 * pi)
+
+
+        # Last Bandwidth Calculation
+
+
+        C3pole = Cgs3*(1. - 1. / A3) + Csb3 + Cgd3B + Cdb3B + Cload
+        R3pole = Rload *  Rl3 / (Rload + Rl3)
+
+        BW3 = 1/(C3pole * R3pole * 2 * pi)
+        Itot = IdL + Id2 + Id3 + (Vdd - Vd1)/RU + (Vd1 - Vss)/RD
+        FOM = Atot * min(BW0, BW1, BW2, BW3)/(Itot * 5)
+        if min(BW0, BW1, BW2, BW3) > FOM_Max:
+            FOM_Max = min(BW0, BW1, BW2, BW3)
+            print FOM, min(BW0, BW1, BW2, BW3)
+            print "Params", Vbias, Vbiasp
+            print "Itot", Itot
+            print "Gain: ", Atot
+            print "BW: ", min(BW0, BW1, BW2, BW3)/10**6
         Vbias += Vbias_inc
     Vbiasp += Vbias_inc
+
